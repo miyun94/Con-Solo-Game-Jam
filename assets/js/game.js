2d3db8f0080328069e32cd7b71880be2b7b943a7
echo <miyunniecho@gmail.com>
2020-12-29 21:31:54 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@ let playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-let enemyName = "Roborto";
+let enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 let enemyHealth = 50;
 let enemyAttack = 12;
 
@@ -18,18 +18,18 @@ let fight = function() {
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
     enemyHealth = enemyHealth - playerAttack; 
 // Log a resulting message to the console so we know that it worked.
-    console.log(playerName + "attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    console.log(playerName + "attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining.");
 // check enemy's health 
     if (enemyHealth <= 0) {
-        window.alert(enemyName + "has died!");
+        window.alert(enemyNames + "has died!");
     }
     else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.")
+        window.alert(enemyNames + " still has " + enemyHealth + " health left.")
     }
 // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
 // Log a resulting message to the console so we know that it worked.
-    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    console.log(enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 // Check player's health
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!"); 
@@ -55,5 +55,15 @@ else {
     window.alert("You need to choose a valid option. Try again!");
 }
 };
+for (let i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
+//let fight = function(enemyName){
 
-fight();
+//}
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
