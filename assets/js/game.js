3023479bee07ffd76a71e574264cca9b2b5a8b5f
echo <miyunniecho@gmail.com>
2021-06-21 21:59:43 -0700

    add option to fight or skip battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,6 +15,7 @@
 let playerName = window.prompt("What is your robot's name?");
 let playerHealth = 100; 
 let playerAttack = 10; 
+let playerMoney = 10; 
 
 console.log(playerName, playerHealth, playerAttack); 
 
@@ -26,6 +27,9 @@ let fight = function(){
     //alert player that they are starting the round
     window.alert("Welcome to Robot Gladiators"); 
 
+    let promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."); 
+
+    if(promptFight === 'fight' || promptFight === "FIGHT"){
     //subtract the value of 'playerAttack' from the value of 'enemyHealth'
     enemyHealth = enemyHealth - playerAttack; 
 
@@ -51,6 +55,23 @@ let fight = function(){
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left."); 
     }
+} else if (promptFight === "skip" || promptFight === "SKIP") {
+    //confirm player wants to skip 
+    let confirmSkip = window.confirm("Are you sure you'd like to quit?"); 
+
+    //if yes, leave fight
+    if (confirmSkip){
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2; 
+        console.log(playerName + " now has " + playerMoney + " remaining."); 
+    } 
+    //if no, ask question again by running fight()
+    else {fight(); 
+    }
+    window.alert(playerName + " has chosen to skip the fight!")
+} else {
+    window.alert("You need to choose a valid option! Try again!"); 
+}
 }; 
 
 //have to call the function for it to run
