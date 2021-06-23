29a729cd42bf2c834a1eb2e1bf28a14c07e0bb45
echo <miyunniecho@gmail.com>
2021-06-23 00:02:09 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,8 @@
 //Semicolor closes out function's code aka its completed
 //Expression : each piece of code separated by semicolons
 //Variable : named location for a value that gets stored in the browser's memory when a program is run
+//Primitive Data : data that only holds one value such as a string, number, or boolean
+//Objects : looser term for a data type that can store more than one value of data such as Arrays
 
 //this creates a function named "fight"
 //declare : create and give function a name
@@ -11,6 +13,11 @@
 //     window.alert("The fight has begun!")
 // }
 
+//Game States
+//"Win" - player robot has defeated all enemy-robots
+//  *fight all enemy-robots
+//  *defeat each enemy-robot
+//"Lose" - player robot's health is zero or less
 
 let playerName = window.prompt("What is your robot's name?");
 let playerHealth = 100; 
@@ -19,11 +26,25 @@ let playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack); 
 
-let enemyName = "Roborto"; 
+let enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]; 
 let enemyHealth = 50;  
 let enemyAttack = 12; 
 
-let fight = function(){
+//displays first name in the list ; arrays always starts at 0
+//console.log(enemyNames[0]); 
+//displays the number of items in the list i.e. 3 in our case
+// console.log(enemyNames.length); 
+
+//Initial Expression : let i=0 ; first statement excuted, initializing the loop iterator or counter
+//Condition Statement : i < 3 ; if this condition evaluates to true, the loop statement execute
+//Increment Expression : i++ ; after the iterator increments, the condition is then reevaluated and continues the loop chain
+// for(let i=0; i<enemyNames.length; i++){
+//     console.log(enemyNames[i]); 
+// }
+
+//Parameter: variable placeholder in the function definition
+
+let fight = function(enemyName){
     //alert player that they are starting the round
     window.alert("Welcome to Robot Gladiators"); 
 
@@ -47,7 +68,7 @@ let fight = function(){
     playerHealth = playerHealth - enemyAttack; 
 
     //log a resulting message to the console 
-    console.log(enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth); 
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth); 
 
     //check player's health
     if (playerHealth <= 0){
@@ -75,4 +96,8 @@ let fight = function(){
 }; 
 
 //have to call the function for it to run
-fight(); 
+//fight(); 
+
+for( let i=0; i < enemyNames.length; i++ ){
+ fight(enemyNames[i]); 
+}
