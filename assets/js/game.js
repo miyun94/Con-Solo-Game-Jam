ab6cd635ddbd7f50418c283b8162a68dd2d70b18
echo <miyunniecho@gmail.com>
2021-06-23 17:41:47 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -117,6 +117,7 @@ for( let i=0; i < enemyNames.length; i++ ){
         fight(pickedEnemyName); 
 
         //if player is still alive and we're not at the last enemy in the array
+        //nameofArray.length - 1 would give us the last index no matter how long the array is  
         if (playerHealth > 0 && i < enemyNames.length -1 ){
             let storeConfirm = window.confirm("The fight is over, visit the store before the next round?"); 
 
@@ -157,3 +158,69 @@ let endGame = function(){
     }
 }; 
 
+//add shop 
+let shop = function(){
+    //ask player what they'd like to do 
+    let shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the shop? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."); 
+
+    //use switch to carry out action
+    switch(shopOptionPrompt){
+        case "refill":
+        case "REFILL":
+            if(playerMoney >=7 ){
+            window.alert("Refilling player's health by 20 for 7 dollars."); 
+
+            //increase health and decrease money 
+            playerHealth = playerHealth + 20; 
+            playerMoney = playerMoney - 7; 
+            } else {
+                window.alert("You don't have enough money!")
+            } 
+            break; 
+ 
+        case "upgrade":
+        case "UPGRADE": 
+            if(playerMoney >=7 ){
+            window.alert("Upgrading player's attack by 6 for 7 dollars"); 
+
+            //increase attack and decrease money
+            playerAttack = playerAttack + 6; 
+            playerMoney = playerMoney - 7; 
+            } else {
+                window.alert("You don't have enough money!")
+            }
+            break; 
+
+        case "leave":
+        case "LEAVE": 
+            window.alert("Leaving the store."); 
+            
+            //do nothing, so function will end
+            break; 
+        
+        default: 
+        window.alert("You did not pick a valid option. Try again"); 
+
+        //call shop() again to force player to pick a valid option 
+        shop(); 
+        break; 
+    }
+}; 
+
+//Example of switch statement
+// var num = 5;
+
+// switch(num) {
+//   case 1:
+//     console.log("the variable was 1");
+//     break;
+//   case 2:
+//     console.log("the variable was 2");
+//     break;
+//   case 3:
+//     console.log("the variable was 3");
+//     break;
+//   default:
+//     console.log("the variable was something else");
+//     break;
+// }
